56ce282a2ff86adde7840a7d6dd026cf23fd0a68
Ysuke <122774311+Yusuke33-4@users.noreply.github.com>
2023-06-27 13:24:36 +0900

    ギミックの追加スクリプト

--- Assets/Oshima/Scripts/Click.cs
+++ Assets/Oshima/Scripts/Click.cs
@@ -7,27 +7,35 @@ public class Click : MonoBehaviour
     //private Material mat;
    // public BoxCollider Box;
     private Rigidbody rb;
+    private bool Gimick=false;
     private void Start()
     {
        // Box = GetComponent<BoxCollider>();
         rb = GetComponent<Rigidbody>();
-        rb.isKinematic = true;//リジットボディをの機能をオフ
+        rb.isKinematic = true;//リジットボディをの機能をオフ       
     }
    
     
     //　押された時
     private void OnMouseDown()
     {
-        rb.isKinematic = true;
+        if (Gimick == false)
+        {
+            rb.isKinematic = true;
+        }
     }
     //　ドラッグされた時
     private void OnMouseDrag()
     {
-        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        transform.position = ray.GetPoint(10f);
+        if (Gimick == false)
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            transform.position = ray.GetPoint(10f);
+        }
     }
     private void OnMouseEnter()//　入ってきた時
     {
+        
         transform.localScale *= 1.5f;
     }
     private void OnMouseExit()  //　出ていったとき
@@ -40,7 +48,8 @@ public class Click : MonoBehaviour
     //}
     private void OnMouseUp()//　離した時
     {
-       // Box.enabled = false;
+        Gimick = true;
+        // Box.enabled = false;
         rb.isKinematic = false;//リジットボディをオン
     }   
     //  private void OnMouseUpAsButton() //　オブジェクト上ではなした時
--- Assets/Oshima/Scripts/Objecttime.cs
+++ Assets/Oshima/Scripts/Objecttime.cs
@@ -6,14 +6,13 @@ using UnityEngine.UI;
 public class Objecttime : MonoBehaviour
 {
     public float Readytime;//始まる時間
-    private Text ReadyText;//
-    public Text timerText;//戦闘での制限時間
-    [SerializeField] Text GoText;//Ready時間が過ぎた時の文字
-    [SerializeField] GameObject Gocall;//Ready時間が過ぎた時のゲームオブジェクト
+    public Text ReadyText;//
+    [SerializeField] Text EndText;//Ready時間が過ぎた時の文字
+    [SerializeField] GameObject Endcall;//Ready時間が過ぎた時のゲームオブジェクト
     // Start is called before the first frame update
     void Start()
     {
-        Gocall.SetActive(false);
+        Endcall.SetActive(false);
         ReadyText = GetComponent<Text>();
     }
 
@@ -23,14 +22,14 @@ public class Objecttime : MonoBehaviour
         if (1 < Readytime)
         {
             Readytime -= Time.deltaTime;
-            ReadyText.text = Readytime.ToString("残り配置時間\n     0");//改行するための空白けさないで
+            ReadyText.text = Readytime.ToString("0");//改行するための空白けさないで
         }
         else if (Readytime <= 1f)
         {
             Destroy(ReadyText);
-            Gocall.SetActive(true);
-            GoText.text = "GO!!";
-            Destroy(GoText, 2f);
+            Endcall.SetActive(true);
+            EndText.text = "終了!!";
+            Destroy(EndText, 2f);
 
         }
     }
