a1465de7a5ce6561cb6709d863a4334c62cba5e1
Radon0 <96102766+Radon0@users.noreply.github.com>
2023-06-29 16:04:52 +0900

    enemy開発

---
+++ Assets/Scripts/Enemy/SetPosition.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPosition : MonoBehaviour
+{
+    //初期位置
+    private Vector3 startPosition;
+    //目的地
+    private Vector3 destination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //初期位置を設定
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
