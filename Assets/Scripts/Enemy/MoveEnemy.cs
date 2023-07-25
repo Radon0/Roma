d23b6c29c0b278e637bd4667bf1607fa1cb976a8
Radon0 <96102766+Radon0@users.noreply.github.com>
2023-07-25 15:04:36 +0900

    仮でMAP変えてます。

--- Assets/Scripts/Enemy/MoveEnemy.cs
+++ Assets/Scripts/Enemy/MoveEnemy.cs
@@ -68,54 +68,54 @@ public class MoveEnemy : MonoBehaviour
         SetState(EnemyState.Walk);
     }
 
-    private void FixedUpdate()
-    {
-        if(isDead)
-        {
-            Debug.Log("死んだ");
-            return;
-        }
-    }
+    //private void FixedUpdate()
+    //{
+    //    if(isDead)
+    //    {
+    //        Debug.Log("死んだ");
+    //        return;
+    //    }
+    //}
     // Update is called once per frame
     void Update()
     {
-        //　見回りまたはキャラクターを追いかける状態
-        if (state == EnemyState.Walk || state == EnemyState.Chase)
-        {
-            //　キャラクターを追いかける状態であればキャラクターの目的地を再設定
-            if (state == EnemyState.Chase)
+            //　見回りまたはキャラクターを追いかける状態
+            if (state == EnemyState.Walk || state == EnemyState.Chase)
             {
-                setPosition.SetDestination(playerTransform.position);
-            }
-            if (enemyController.isGrounded)
-            {
-                velocity = Vector3.zero;
-                animator.SetFloat("Speed", 2.0f);
-                direction = (setPosition.GetDestination() - transform.position).normalized;
-                transform.LookAt(new Vector3(setPosition.GetDestination().x, transform.position.y, setPosition.GetDestination().z));
-                velocity = direction * walkSpeed;
-            }
+                //　キャラクターを追いかける状態であればキャラクターの目的地を再設定
+                if (state == EnemyState.Chase)
+                {
+                    setPosition.SetDestination(playerTransform.position);
+                }
+                if (enemyController.isGrounded)
+                {
+                    velocity = Vector3.zero;
+                    animator.SetFloat("Speed", 2.0f);
+                    direction = (setPosition.GetDestination() - transform.position).normalized;
+                    transform.LookAt(new Vector3(setPosition.GetDestination().x, transform.position.y, setPosition.GetDestination().z));
+                    velocity = direction * walkSpeed;
+                }
 
-            //　目的地に到着したかどうかの判定
-            if (Vector3.Distance(transform.position, setPosition.GetDestination()) < 3.0f)
-            {
-                SetState(EnemyState.Wait);
-                animator.SetFloat("Speed", 0.0f);
+                //　目的地に到着したかどうかの判定
+                if (Vector3.Distance(transform.position, setPosition.GetDestination()) < 3.0f)
+                {
+                    SetState(EnemyState.Wait);
+                    animator.SetFloat("Speed", 0.0f);
+                }
+                //　到着していたら一定時間待つ
             }
-            //　到着していたら一定時間待つ
-        }
-        else if (state == EnemyState.Wait)
-        {
-            elapsedTime += Time.deltaTime;
+            else if (state == EnemyState.Wait)
+            {
+                elapsedTime += Time.deltaTime;
 
-            //　待ち時間を越えたら次の目的地を設定
-            if (elapsedTime > waitTime)
-            {
-                SetState(EnemyState.Walk);
+                //　待ち時間を越えたら次の目的地を設定
+                if (elapsedTime > waitTime)
+                {
+                    SetState(EnemyState.Walk);
+                }
             }
-        }
-        velocity.y += Physics.gravity.y * Time.deltaTime;
-        enemyController.Move(velocity * Time.deltaTime);
+            velocity.y += Physics.gravity.y * Time.deltaTime;
+            enemyController.Move(velocity * Time.deltaTime);
     }
 
     //　敵キャラクターの状態変更メソッド
@@ -146,31 +146,31 @@ public class MoveEnemy : MonoBehaviour
     }
 
     //被ダメージ処理
-    public void Damage(int value)
-    {
-        if (value <= 0)
-        {
-            return;
-        }
+    //public void Damage(int value)
+    //{
+    //    if (value <= 0)
+    //    {
+    //        return;
+    //    }
 
-        EnemyHp -= value;
+    //    EnemyHp -= value;
 
-        if (EnemyHp <= 0)
-        {
-            Dead();
-        }
-    }
+    //    if (EnemyHp <= 0)
+    //    {
+    //        Dead();
+    //    }
+    //}
     //　敵キャラクターの状態取得メソッド
     public EnemyState GetState()
     {
         return state;
     }
     //死亡時の処理
-    void Dead()
-    {
-        isDead = true;
-        //boxCollider.enabled = false;
-        //animator.SetBool(DeadHash, true);
+    //void Dead()
+    //{
+    //    isDead = true;
+    //    //boxCollider.enabled = false;
+    //    //animator.SetBool(DeadHash, true);
 
-    }
+    //}
 }
