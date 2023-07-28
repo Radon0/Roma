e8bd12e5fced658d8a54efc35982967bd09ba80d
Ysuke <122774311+Yusuke33-4@users.noreply.github.com>
2023-07-28 14:38:57 +0900

    注射器にLOGがちゃんとでるように

--- Assets/Oshima/Scripts/EventTag.cs
+++ Assets/Oshima/Scripts/EventTag.cs
@@ -12,14 +12,18 @@ public class EventTag : MonoBehaviour
 
 	private void OnCollisionEnter(Collision collision)
 	{
-		if (collision.gameObject.tag == "Syringes")
+		//if (collision.gameObject.tag == "Syringes")
+		//{
+		//	logSystem.AddLogText("<color=blue>注射器"+"</color>"+"が出現!回復のチャンスだ!!", LogInfomation.LogType.Event);
+		//}
+		if (collision.gameObject.tag == "Item")
 		{
-			logSystem.AddLogText("<color=blue>注射器"+"</color>"+"が出現!回復のチャンスだ!!", LogInfomation.LogType.Event);
+			logSystem.AddLogText("<color=blue>"+collision.gameObject.GetComponent<ItemName>().syringesName + "</color>" + "が出現!回復のチャンスだ!!", LogInfomation.LogType.Event);
 		}
-		if (collision.gameObject.tag=="Enemy")
-		{		
-			logSystem.AddLogText("<color=green>"+collision.gameObject.GetComponent<EnemyInfomation>().EnemyName+"</color>"+"が<color=blue>" + areaName + "</color>" + "に接触しました。", LogInfomation.LogType.Event);
-		}
+		//if (collision.gameObject.tag=="Enemy")
+		//{		
+		//	logSystem.AddLogText("<color=green>"+collision.gameObject.GetComponent<EnemyInfomation>().EnemyName+"</color>"+"が<color=blue>" + areaName + "</color>" + "に接触しました。", LogInfomation.LogType.Event);
+		//}
         if (collision.gameObject.tag == "Player")
         {
             logSystem.AddLogText("<color=green>"+collision.gameObject.GetComponent<PlayerInfomation>().PlayerName + "</color>" + "が<color=blue>" + areaName + "</color>" + "に接触しました。", LogInfomation.LogType.Event);
