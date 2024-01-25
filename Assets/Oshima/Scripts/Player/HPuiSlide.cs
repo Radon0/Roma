6a789174fe2ec368d958ab497a81e9f78e606bc4
Ysuke <122774311+Yusuke33-4@users.noreply.github.com>
2024-01-25 17:54:46 +0900

    HPUIの作成

--- Assets/Oshima/Scripts/Player/HPuiSlide.cs
+++ Assets/Oshima/Scripts/Player/HPuiSlide.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Hpui : MonoBehaviour
+public class HPuiSlide : MonoBehaviour
 {
-     //Slider HpSlider;
+    Slider HpSlider;
     private Image _image;
     void Start() // Start is called before the first frame update
     {
-        //HpSlider = GetComponent<Slider>();
         _image = this.GetComponent<Image>();
     }
 
     public void HPUI(float Hp)
     {
-        //HpSlider.value = Hp;
-        _image.fillAmount = Hp / 100f;
+        HpSlider.value = Hp;
+
     }
+
 }
